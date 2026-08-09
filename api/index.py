2630eb49f48b78c2e81445ef572aad3f68f3cc3e
lohveraofficial <lohveraofficial@gmail.com>
2026-08-09 20:34:19 +0530

    Add CORS support for external apps

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
@@ -12,6 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 # --- Global Initialization for Serverless Optimization ---
 db = None
